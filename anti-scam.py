b01d9579e1630e7cbd59605ce8659a44ff4bfd77
ventsislavkovachev <ventsislav.kovachev@estiam.com>
2025-04-17 14:20:58 +0200

    Better documentation

--- anti-scam.py
+++ anti-scam.py
@@ -1,3 +1,5 @@
+import random
+import time
 from huggingface_hub import InferenceClient
 from google.cloud import texttospeech
 from google.cloud import speech
@@ -12,7 +14,9 @@ from pygame import mixer
 with open("nebius_api_key.txt", "r") as key_file:
     nebius_api_key = key_file.read().strip()
 
-llm_client = InferenceClient(
+# Initialize the InferenceClient with the NEBIUS API key
+# This client will be used to generate AI responses
+victim_llm_client = InferenceClient(
     provider="nebius",
     api_key=nebius_api_key,
 )
@@ -21,12 +25,14 @@ llm_client = InferenceClient(
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="google_key.json"
 
 # Donner le contexte général
+## Ce contexte est utilisé pour définir le rôle de l'IA
 context =\
 "Tu es une fausse victime d'arnaque." \
 "Tu sert à faire perdre du temps à un arnaqueur." \
 "Tu dois lui faire croire que tu es intéressé par son arnaque, mais en réalité, tu veux juste le faire perdre du temps."
 
 # Donner la description de l'arnaque
+## Cette description est utilisée pour définir le type d'escroc et le type d'arnaque
 arnaque =\
 "L'arnaque consiste à faire croire à la victime qu'elle a gagné un prix, mais qu'elle doit payer des frais pour le récupérer." \
 "Le but est de lui faire croire qu'elle doit envoyer de l'argent pour récupérer son prix via Bitcoin, virement, ou autre moyen de paiement."
@@ -56,6 +62,7 @@ negative =\
 "Tu jouera l'incrédulité voir l'ignorance." \
 "Si l'escroc te demande si tu es une IA et insiste tu dois t'enerver et te sentir insulté."
 
+# Donner des instructions sur les lapsus et les erreurs de language
 lapsus =\
 "Tu dois faire des lapsus et des erreurs de language." \
 "Tu parles à voix haute donc tu ajoutes les pauses et hésitation sous la forme" \
@@ -71,6 +78,8 @@ voix =\
 "Tu dois sourire en parlant." \
 "Tu dois être très enthousiaste et très intéressé par ce que dit l'escroc."
 
+# Initialize the messages list with the system message
+# This is the initial message that sets the context for the conversation
 messages=[
         {
             "role": "system",
@@ -85,109 +94,179 @@ messages=[
         }
     ]
 
-mixer.init()
+def write_flac_audio_file(file, data):
+    # Save the audio data to a FLAC file
+    sf.write(file, data, sd.default.samplerate)
 
-while True:
+def write_mp3_audio_file(file, data):
+    # Save the audio data to an MP3 file
+    with open(file, 'wb') as out:
+        out.write(data)
+
+# Function to record audio from the microphone and save it to a file
+def escroc_get_microphone_input():
+    # Initialize the microphone
+    sd.default.samplerate = 44100
+    sd.default.channels = 1
+
     # Record audio from the microphone
     duration = 5  # seconds
-    sample_rate = 44100  # Hz
     print("Recording...")
-    #print(sd.query_devices())
-    #sd.default.device = 12
-    audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1, dtype='int16')
+    audio_data = sd.rec(int(duration * sd.default.samplerate),
+                         samplerate=sd.default.samplerate, channels=1, dtype='int16')
     sd.wait()  # Wait until recording is finished
     print("Recording finished.")
 
     # Save the recorded audio to a file
-    sf.write('escroc.flac', audio_data, sample_rate)
-    
-    #user_input = stt_client.automatic_speech_recognition("escroc.flac", model="openai/whisper-large-v3")
-    
+    write_flac_audio_file('escroc.flac', audio_data)
+
+# Function to convert the recorded audio to text using Google Cloud Speech-to-Text API
+# This function will be called recursively if no voice is detected
+def escroc_speech_to_text():
+    # Initialize the speech client and load the audio file
     speech_client = speech.SpeechClient()
     with open("escroc.flac", "rb") as audio_file:
         content = audio_file.read()
+    
+    # Create the audio object and configuration
     audio = speech.RecognitionAudio(content=content)
+
+    # Set the configuration for the speech recognition
     config = speech.RecognitionConfig(
-        #encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
         sample_rate_hertz=44100,
         language_code="fr-FR",
     )
+
+    # Perform the speech recognition
+    # The response is a long-running operation, so we need to wait for it to finish
     speech_result = speech_client.recognize(config=config, audio=audio)
-    
+
+    # Chech if the escroc said something
     if (len(speech_result.results) == 0):
         print("Aucune voix détectée.")
-        continue
-    user_input = speech_result.results[0].alternatives[0].transcript
-    print("Escroc : ", user_input)
-    
+        escroc_speech_to_text()
+
+    return speech_result
+
+# Function to save the message history in a list
+# This function will be called after each message is sent or received
+def save_message_history(role, input):
+    # Save the message history in a list
+    # This is a list of dictionaries, where each dictionary contains the role and content of the message
     messages.append(
         {
-            "role": "user",
-            "content": user_input,
+            "role": role,
+            "content": input,
         }
     )
 
-    substring = "stop"
-    if substring in user_input:
-        break
-    #else:
-        #print(f'"{user_input}" does not contain "{substring}"')
-
-    completion = llm_client.chat.completions.create(
+# Function to get AI input from the victim using the InferenceClient
+def victim_get_AI_input(messages):
+    completion = victim_llm_client.chat.completions.create(
         model="Qwen/Qwen2.5-32B-Instruct",
         messages=messages,
         max_tokens=500,
         stream=True,
         temperature=0.6
     )
+    
+    return completion
 
-    reponse = ""
-    print("Victim : ")
-    for chunk in completion:
-        reponse += chunk.choices[0].delta.content
-        print(chunk.choices[0].delta.content, end="")
-    print()
-    
-    messages.append(
-        {
-            "role": "assistant",
-            "content": reponse,
-        }
-    )
-    
+# This function will be called after the escroc input is received
+# and the AI input is generated
+def victim_AI_text_to_speach(victim_input):
+    # Initialize the text-to-speech client
+    # This client will be used to convert the text to speech
     tts_client = texttospeech.TextToSpeechClient()
 
     # Set the text input to be synthesized
-    synthesis_input = texttospeech.SynthesisInput(text=reponse)
+    synthesis_input = texttospeech.SynthesisInput(text=victim_input)
 
-    # Build the voice request, select the language code ("en-US") 
-    # ****** the NAME
-    # and the ssml voice gender ("neutral")
+    # Select the type of voice you want to use
     voice = texttospeech.VoiceSelectionParams(
         language_code='fr-FR',
         name='fr-FR-Chirp-HD-O',
         ssml_gender=texttospeech.SsmlVoiceGender.FEMALE)
 
     # Select the type of audio file you want returned
+    # In this case, we are using MP3 format
     audio_config = texttospeech.AudioConfig(
         audio_encoding=texttospeech.AudioEncoding.MP3)
 
-    # Perform the text-to-speech request on the text input with the selected
-    # voice parameters and audio file type
-    response = tts_client.synthesize_speech(input = synthesis_input, voice = voice, audio_config = audio_config)
-
+    # Perform the text-to-speech request on the text input with the selected voice parameters and audio file type
     # The response's audio_content is binary.
-    with open('victim.mp3', 'wb') as out:
-        # Write the response to the output file.
-        out.write(response.audio_content)
-        print('Audio content written to file "victim.mp3"')
+    synthesize_speech = tts_client.synthesize_speech(input = synthesis_input,
+                                             voice = voice,
+                                               audio_config = audio_config)
+                                               
+    return synthesize_speech
 
-    # Play the audio file victim.mp3
-    mixer.music.load('victim.mp3')
+# Function to play the audio file using Pygame mixer
+# This function will be called after victim the audio file is generated
+def victim_play_audio_file(file):
+    # Initialize the mixer and load the audio file
+    mixer.music.load(file)
     mixer.music.play()
 
+    # Wait for the music to finish playing
+    # This is a blocking call, so the program will wait here until the music is done playing
     while mixer.music.get_busy():
         # Wait for the music to finish playing
         pass
+
+    # Stop the mixer and unload the audio file
     mixer.music.stop()
     mixer.music.unload()
+
+# Initialize the Pygame mixer
+# This is used to play the audio file generated by the TTS client
+mixer.init()
+
+# Main loop to continuously listen for the escroc input and generate AI responses
+# This loop will run until the escroc says "stop"
+while True:
+    # Get input from the escroc (microphone)
+    escroc_get_microphone_input()
+    
+    # Get escroc audio file and generate text from it
+    escroc_input = escroc_speech_to_text()
+
+    # Get transcript of the escroc input
+    escroc_transcript = escroc_input.results[0].alternatives[0].transcript
+    print("Escroc : ", escroc_transcript)
+    
+    # Save the escroc message history
+    save_message_history("user", escroc_transcript)
+
+    # Check if the escroc said "stop" to break the loop
+    # This is a simple check to see if the escroc said "stop"
+    substring = "stop"
+    if substring in escroc_transcript:
+        break
+
+    # Get AI input from the victim
+    victim_input = victim_get_AI_input(messages)
+
+    victim_content = ""
+    print("Victim : ")
+    for chunk in victim_input:
+        victim_content += chunk.choices[0].delta.content
+        print(chunk.choices[0].delta.content, end="")
+        
+        # Wait for random milliseconds between each chunk to simulate a human typing speed
+        # This is not a blocking call, so the program will continue to run while waiting
+        ms=random.randint(0, 9)/1000
+        time.sleep(ms)
+    print()
+    
+    # Save the AI message history
+    save_message_history("assistant", victim_content)
+    
+    # Generate the audio file from the AI text 
+    victim_tts = victim_AI_text_to_speach(victim_content)
+
+    # and save it to a file
+    write_mp3_audio_file('victim.mp3', victim_tts.audio_content)
+
+    # Play the audio file
+    victim_play_audio_file('victim.mp3')
